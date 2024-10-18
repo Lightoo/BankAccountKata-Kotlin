931602378a011209f12f020316ce803f0fecc7c8
Abdellatif AZIKI <abdellatifaziki@gmail.com>
2024-10-18 08:07:52 +0200

    US 1: add unit test 2 for deposit account

--- src/test/kotlin/org/kata/bankaccount/services/AccountServiceTest.kt
+++ src/test/kotlin/org/kata/bankaccount/services/AccountServiceTest.kt
@@ -1,5 +1,6 @@
 package org.kata.bankaccount.services
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -18,5 +19,12 @@ class AccountServiceTest {
         val account = Account()
         accountService.deposit(account, 200.0)
         assertEquals(200.0, account.balance)
+    }
+    @Test
+    fun deposit_with_negative_amount() {
+        val account = Account()
+        Assertions.assertThrows(
+            Exception::class.java
+        ) { accountService.deposit(account, -200.0) }
     }
 }
