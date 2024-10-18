d82137053327911cf81127c228ccce2ccbbe9c82
Abdellatif AZIKI <abdellatifaziki@gmail.com>
2024-10-18 08:10:23 +0200

    US 2: add 2 unit tests withdraw account

--- src/test/kotlin/org/kata/bankaccount/services/AccountServiceTest.kt
+++ src/test/kotlin/org/kata/bankaccount/services/AccountServiceTest.kt
@@ -27,4 +27,20 @@ class AccountServiceTest {
             Exception::class.java
         ) { accountService.deposit(account, -200.0) }
     }
+    @Test
+    @Throws(Exception::class)
+    fun withdraw_with_sufficient_amount() {
+        val account = Account(2000.0, ArrayList())
+        accountService.withdraw(account, 500.0)
+        assertEquals(1500.0, account.balance)
+        assertEquals(1, account.transactions.size)
+    }
+
+    @Test
+    fun withdraw_with_insufficient_amount() {
+        val account = Account(1000.0, ArrayList())
+        Assertions.assertThrows(
+            Exception::class.java
+        ) { accountService.withdraw(account, 2000.0) }
+    }
 }
